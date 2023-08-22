281c620c3464da8d8033d44b723bce6a2da2a2d9
Brendon Otto <brendon@brendonotto.com>
2023-08-21 21:02:20 -0500

    Axum hello world

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    println!("--> Listening on {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
 }
